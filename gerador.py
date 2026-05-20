e9189545dfab820d92954290d9677948f263fd2d
Pietro <pietrofernandesstefanuto@gmail.com>
2026-05-20 18:51:47 -0300

    adiciona opção de símbolos e validação de input
    Pergunta ao usuário se deseja incluir símbolos na senha.
    Adicionado try/except com while True para validar o input do tamanho.

---
+++ gerador.py
@@ -0,0 +1,19 @@
+import random
+import string
+
+while True:
+    print("Bem-vindo ao Gerador de Senhas!")
+    pontuacao = input('Você gostaria de utilizar pontuação na sua senha? (s/n): ')
+    try:
+        if pontuacao == 's':
+            caracteres = string.ascii_letters + string.digits + string.punctuation
+        else:
+            caracteres = string.ascii_letters + string.digits
+
+        tamanho_senha = int(input("Digite o tamanho da senha desejada: "))
+
+        senha = ''.join(random.choice(caracteres) for i in range(tamanho_senha))
+        print("Senha gerada:", senha)
+        break
+    except ValueError:
+        print("Por favor, digite um número válido.")
